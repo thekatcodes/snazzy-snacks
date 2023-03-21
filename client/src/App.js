665a7535b62f9a365671085d333af0160f52354c
Katie Liu <katieliu59@gmail.com>
2023-03-21 14:37:35 -0400

    Add checkout form component to App for testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
-import Login from './components/Login';
+import React, { useState } from "react";
+import "./App.css";
+import CheckoutForm from "./components/CheckoutForm";
+import Login from "./components/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+	// const [showItem, setShowItem] = useState(false);
+
+	return (
+		<div className="App">
+            <Login />
+            <CheckoutForm />
+        </div>
+    )
 }
 
 export default App;
