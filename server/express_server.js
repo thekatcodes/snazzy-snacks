024cc136aa6924dcab5d2b3eb04448aaeb87440b
Katie Liu <katieliu59@gmail.com>
2023-03-21 14:35:46 -0400

    Integrate Stripe checkout session

--- server/express_server.js
+++ server/express_server.js
@@ -1,22 +1,58 @@
 const pool = require("./db/index");
+require("dotenv").config();
+
+const stripe = require("stripe")(
+	process.env.STRIPE_SECRET_KEY
+);
 
 const express = require("express");
+const e = require("express");
 const app = express();
 const PORT = 8080;
 
-app.get("/api", async(req, res) => {
-  try {
-    const users = await (pool.query(` 
+const YOUR_DOMAIN = "http://localhost:8080";
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/api", async (req, res) => {
+	try {
+		const users = await pool.query(` 
       SELECT *
       FROM users
-    `))
-    res.json(users.rows);
-  }
-  catch (error) { 
-    console.log(error);
-  }
+    `);
+		res.json(users.rows);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+app.post("/create-checkout-session", async (req, res) => {
+	console.log("test");
+	console.log(req.body);
+	const { priceId } = req.body;
+
+	const session = await stripe.checkout.sessions.create({
+		line_items: [
+			{
+				price: priceId,
+				quantity: 1,
+			},
+		],
+		mode: "subscription",
+		success_url: `${YOUR_DOMAIN}?success=true`,
+		cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+		billing_address_collection: "required",
+		shipping_address_collection: {
+			allowed_countries: ["CA"],
+		},
+	});
+
+	// res.json('test');
+	// res.json({ id: session.id });
+	res.redirect(303, session.url);
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+	console.log(`Example app listening on port ${PORT}!`);
 });
