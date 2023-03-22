497ca06cd3ecc43c4d0c9079c9d76162d1e8b857
Katie Liu <katieliu59@gmail.com>
2023-03-22 12:31:07 -0400

    Update order confirmation code

--- client/src/components/OrderConfirmation.js
+++ client/src/components/OrderConfirmation.js
@@ -3,12 +3,17 @@ import React from "react";
 
 
 
-function Subscriptions() {
+function OrderConfirmation() {
     return (
         <>
-        <h1>Your order is complete!</h1>
+            <h1>Your order is complete!</h1>
+            <div>
+                <h3>Order number: </h3>
+                <h3>Email address </h3>
+            </div>
+            <button>View order history</button>
         </>
     )
 }
 
-export default Subscriptions;
+export default OrderConfirmation;
