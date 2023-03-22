1a87e9b8d75a6f8b64af8ab80ffac0f4ffe9306d
Katie Liu <katieliu59@gmail.com>
2023-03-22 11:52:26 -0400

    Create Order Confirmation component skeleton

---
+++ client/src/components/OrderConfirmation.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+
+function Subscriptions() {
+    return (
+        <>
+        <h1>Your order is complete!</h1>
+        </>
+    )
+}
+
+export default Subscriptions;
