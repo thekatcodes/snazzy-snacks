fe58755ddf2cbe1836a3ee8b7444e1038b1344db
Katie Liu <katieliu59@gmail.com>
2023-03-23 11:41:18 -0400

    Update order confirmation component to query database

--- client/src/components/OrderConfirmation.js
+++ client/src/components/OrderConfirmation.js
@@ -1,20 +1,42 @@
-import React from "react";
 
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
+function OrderConfirmation() {
+  const [userData, setUserData] = useState(null);
 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+          const response = await axios.get("/api");
+          console.log(response.data)
+        setUserData(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
 
-function OrderConfirmation(props) {
-    console.log(props.cookieValue)
-    return (
-        <>
-            <h1>Your order is complete!</h1>
-            <div>
-                <h3>Order number: </h3>
-                <h3>Email address </h3>
-            </div>
-            <button>View order history</button>
-        </>
-    )
+  return (
+    <>
+      <h1>Your order is complete!</h1>
+      <div>
+        <p>Order number: </p>
+        {userData && (
+          <>
+            <p>Email address: {userData.email}</p>
+            <p>
+              Shipping address: {userData.street}, {userData.city},{" "}
+              {userData.province}, {userData.country} {userData.postal_code}
+            </p>
+          </>
+        )}
+        <p>Order summary:</p>
+      </div>
+      <button>View order history</button>
+    </>
+  );
 }
 
 export default OrderConfirmation;
