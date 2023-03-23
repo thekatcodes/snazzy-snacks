f039d0e9e45c43e7c534af68af078d772bdb83c9
Katie Liu <katieliu59@gmail.com>
2023-03-23 01:11:35 -0400

    Create order_confirmation query helper functions

---
+++ server/order_confirmation.js
@@ -0,0 +1,29 @@
+const pool = require("./db/index");
+
+// Grab users table from PSQL
+async function getUsers() {
+  try {
+    const users = await pool.query(`
+      SELECT * 
+      FROM users;
+    `);
+    return users.rows;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Updates users table with address
+async function updateAddress(street, city, province, country, postalCode) {
+    try {
+      await pool.query(`
+        UPDATE users SET street=$1, city=$2, province=$3, country=$4, postal_code=$5
+        WHERE id=6
+      `, [street, city, province, country, postalCode]);
+      return "Address updated successfully";
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+module.exports = { getUsers, updateAddress };
