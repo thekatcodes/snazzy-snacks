cd8b118416b8f6d72b8fd3f267f9dc34d7d7a529
Katie Liu <katieliu59@gmail.com>
2023-03-23 13:44:51 -0400

    Update query for order confirmation

--- server/db/queries/sql-queries/04_order_summary.sql
+++ server/db/queries/sql-queries/04_order_summary.sql
@@ -1,6 +1,11 @@
-SELECT u.id, u.email, u.street, u.city, u.province, u.country, u.postal_code, b.id AS box_id, p.tier
-FROM users u
-INNER JOIN boxes b ON u.id = b.customer_id
-INNER JOIN selections s ON b.id = s.box_id
-INNER JOIN products p ON s.product_id = p.id
-Where u.id=
+-- SELECT u.id, u.email, u.street, u.city, u.province, u.country, u.postal_code, b.id AS box_id, p.tier
+-- FROM users u
+-- INNER JOIN boxes b ON u.id = b.customer_id
+-- INNER JOIN selections s ON b.id = s.box_id
+-- INNER JOIN products p ON s.product_id = p.id
+-- Where u.id=
+
+SELECT users.id AS user_id, email, street, city, province, country, postal_code, subscription_tier, boxes.id AS order_number
+FROM users
+INNER JOIN boxes ON users.id = boxes.customer_id
+WHERE users.id=1;
