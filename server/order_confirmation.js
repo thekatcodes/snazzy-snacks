a9ffdbf54db41e0ac7824998f569ef7400ea2caa
Katie Liu <katieliu59@gmail.com>
2023-03-23 13:46:17 -0400

    Add function that creates order number and update findUserId query

--- server/order_confirmation.js
+++ server/order_confirmation.js
@@ -1,13 +1,15 @@
 const pool = require("./db/index");
 
-// Grab users table from PSQL
-async function getUsers() {
+// Find user id of user who made a successful purchase by email
+async function findUserId(email) {
   try {
     const users = await pool.query(`
       SELECT * 
-      FROM users;
-    `);
-    return users.rows;
+      FROM users
+      WHERE email LIKE $1
+    `, [email]);
+      console.log("USER ID:", users.id)
+    return users.id;
   } catch (error) {
     console.log(error);
   }
@@ -24,6 +26,20 @@ async function updateAddress(street, city, province, country, postalCode, email)
     } catch (err) {
       console.log(err);
     }
+}
+  
+// Updates boxes table with new row (order number)
+async function createOrderNumber(street, city, province, country, postalCode, email) {
+    try {
+      await pool.query(`
+      INSERT INTO boxes (customer_id, order_date)
+      VALUES (**ID NMBR**, CURRENT_DATE);
+      `, [street, city, province, country, postalCode, email]);
+      return "Order number created successfully";
+    } catch (err) {
+      console.log(err);
+    }
   }
 
-module.exports = { getUsers, updateAddress };
+
+module.exports = { findUserId, updateAddress, createOrderNumber };
