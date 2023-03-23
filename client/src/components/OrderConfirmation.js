560d7c6bdbb3d7a097a4f676c7b038158cf835d0
Katie Liu <katieliu59@gmail.com>
2023-03-23 18:38:38 -0400

    Refactor code and implement logic to render data appropriate to the user and recent order

--- client/src/components/OrderConfirmation.js
+++ client/src/components/OrderConfirmation.js
@@ -1,43 +1,59 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Footer from './Footer'
+import Footer from "./Footer";
 
-function OrderConfirmation() {
-  const [userData, setUserData] = useState(null);
+function OrderConfirmation(props) {
+	console.log(props.cookieValue); // -> first_name
+	const [userData, setUserData] = useState(null);
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await axios.get("/order-summary");
-        console.log("order confirmation response:", response.data);
-        setUserData(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    fetchData();
-  }, []);
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const response = await axios.get("/order-summary");
+				console.log("order confirmation response:", response.data);
+				setUserData(response.data);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		fetchData();
+	}, []);
 
-  return (
-    <>
-      <h1>Your order is complete!</h1>
-      <div>
-        {userData && userData.map((order, index) => (
-            <div key={index}>
-              <p>Order number: </p>
-            <p>Email address: {order.email}</p>
-            <p>
-              Shipping address: {order.street}, {order.city},{" "}
-              {order.province}, {order.country} {order.postal_code}
-            </p>
-          </div>
-        ))}
-        <p>Order summary:</p>
-      </div>
-          <button>View order history</button>
-          <Footer />
-    </>
-  );
+	return (
+		<>
+			<h1>Your order is complete!</h1>
+			<div>
+				{userData &&
+          userData.map((order, index) => {
+            
+						// Filter orders by first name & cookieValue
+            if (order.first_name === props.cookieValue) {
+
+              // In case a user has previous purchases, at new checkout, the data will be filtered to render the highest order number (aka most recent order)
+              const maxOrderNumber = Math.max(...userData.map(order => order.order_number));
+              const mostRecentOrder = userData.find(order => order.order_number === maxOrderNumber);
+              console.log(mostRecentOrder);
+
+							return (
+								<div key={index}>
+									<p>Order number: {mostRecentOrder.order_number}</p>
+									<p>Email address: {mostRecentOrder.email}</p>
+									<p>
+										Shipping address: {mostRecentOrder.street}, {mostRecentOrder.city},{" "}
+										{mostRecentOrder.province}, {mostRecentOrder.country} {mostRecentOrder.postal_code}
+									</p>
+									<p>Order summary: {mostRecentOrder.price}</p>
+								</div>
+							);
+						}
+						// If order doesn't match the if condition, return null
+						return null;
+					})}
+			</div>
+			<button>View order history</button>
+			<Footer />
+		</>
+	);
 }
 
 export default OrderConfirmation;
