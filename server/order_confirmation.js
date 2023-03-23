4daf15694f9280d9024e49174045ade698f18131
Katie Liu <katieliu59@gmail.com>
2023-03-23 15:31:22 -0400

    Fix bug retrieving user id, update updateAddress function

--- server/order_confirmation.js
+++ server/order_confirmation.js
@@ -2,26 +2,26 @@ const pool = require("./db/index");
 
 // Find user id of user who made a successful purchase by email
 async function findUserId(email) {
-  try {
-    const users = await pool.query(`
-      SELECT * 
-      FROM users
-      WHERE email=$1
-    `, [email]);
-      console.log("USER ID:", users.id)
-    return users.rows[0].id;
-  } catch (error) {
-    console.log(error);
+    try {
+      const user = await pool.query(`
+        SELECT id
+        FROM users
+        WHERE email=$1
+      `, [email]);
+      console.log("USER ID:", user.rows[0].id);
+      return user.rows[0].id;
+    } catch (error) {
+      console.log(error);
+    }
   }
-}
 
 // Updates users table with address
-async function updateAddress(street, city, province, country, postalCode, subscriptionId, email) {
+async function updateAddress(street, city, province, country, postalCode, subscriptionTier, subscriptionId, email) {
     try {
       await pool.query(`
-        UPDATE users SET street=$1, city=$2, province=$3, country=$4, postal_code=$5, stripe_sub_id=$6 
-        WHERE email LIKE $7
-      `, [street, city, province, country, postalCode, subscriptionId, email]);
+        UPDATE users SET street=$1, city=$2, province=$3, country=$4, postal_code=$5, subscription_tier=$6, stripe_sub_id=$7 
+        WHERE email LIKE $8
+      `, [street, city, province, country, postalCode, subscriptionId, subscriptionTier, email]);
       return "Address updated successfully";
     } catch (err) {
       console.log(err);
@@ -29,17 +29,20 @@ async function updateAddress(street, city, province, country, postalCode, subscr
 }
   
 // Updates boxes table with new row (order number)
-async function createOrderNumber(userId) {
-    try {
-      await pool.query(`
-      INSERT INTO boxes (customer_id)
-      VALUES ($1);
-      `, [userId]);
-      return "Order number created successfully";
-    } catch (err) {
-      console.log(err);
-    }
-  }
+// async function createOrderNumber(userId) {
+//     try {
+//       await pool.query(`
+//       INSERT INTO boxes (customer_id)
+//       VALUES ($1);
+//       `, [userId]);
+        
+//       return "Order number created successfully";
+//     } catch (err) {
+//       console.log(err);
+//     }
+// }
+
+
 
 
 module.exports = { findUserId, updateAddress, createOrderNumber };
