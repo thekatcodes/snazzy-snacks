072027c8a7a21c4284f4dc51568e23676b0fe565
Katie Liu <katieliu59@gmail.com>
2023-03-23 16:14:43 -0400

    Update query

--- server/db/queries/sql-queries/04_order_summary.sql
+++ server/db/queries/sql-queries/04_order_summary.sql
@@ -1,5 +1,5 @@
 SELECT users.id AS user_id, email, street, city, province, country, postal_code, subscription_tier, boxes.id AS order_number
 FROM users
 INNER JOIN boxes ON users.id = boxes.customer_id
-WHERE users.id=1
+-- WHERE users.id=1
 ORDER BY order_number DESC;
