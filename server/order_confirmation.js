efb8cac8e10d478cbcaec0387f948e9425a8ae82
Katie Liu <katieliu59@gmail.com>
2023-03-23 14:19:07 -0400

    Update queries

--- server/order_confirmation.js
+++ server/order_confirmation.js
@@ -6,22 +6,22 @@ async function findUserId(email) {
     const users = await pool.query(`
       SELECT * 
       FROM users
-      WHERE email LIKE $1
+      WHERE email=$1
     `, [email]);
       console.log("USER ID:", users.id)
-    return users.id;
+    return users.rows[0].id;
   } catch (error) {
     console.log(error);
   }
 }
 
 // Updates users table with address
-async function updateAddress(street, city, province, country, postalCode, email) {
+async function updateAddress(street, city, province, country, postalCode, subscriptionId, email) {
     try {
       await pool.query(`
-        UPDATE users SET street=$1, city=$2, province=$3, country=$4, postal_code=$5
-        WHERE email LIKE $6
-      `, [street, city, province, country, postalCode, email]);
+        UPDATE users SET street=$1, city=$2, province=$3, country=$4, postal_code=$5, stripe_sub_id=$6 
+        WHERE email LIKE $7
+      `, [street, city, province, country, postalCode, subscriptionId, email]);
       return "Address updated successfully";
     } catch (err) {
       console.log(err);
@@ -29,12 +29,12 @@ async function updateAddress(street, city, province, country, postalCode, email)
 }
   
 // Updates boxes table with new row (order number)
-async function createOrderNumber(street, city, province, country, postalCode, email) {
+async function createOrderNumber(userId) {
     try {
       await pool.query(`
-      INSERT INTO boxes (customer_id, order_date)
-      VALUES (**ID NMBR**, CURRENT_DATE);
-      `, [street, city, province, country, postalCode, email]);
+      INSERT INTO boxes (customer_id)
+      VALUES ($1);
+      `, [userId]);
       return "Order number created successfully";
     } catch (err) {
       console.log(err);
