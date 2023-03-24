2a5e50fc4ff140f17066c14a1f99904f9cc0cc46
--replace-all <samma118@hotmail.com>
2023-03-24 02:12:28 -0400

    Finish draft of subscriptions component

---
+++ client/src/components/SnackPreview.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+
+import "./styles/SnackPreview.scss";
+import classNames from "classnames";
+
+
+export default function SnackPreview(props) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("/products")
+      .then(response => {
+        setProducts(response.data)
+      })
+      .catch(error => console.log(error))
+  }, []);
+
+  // console.log(products);
+
+  const productList = products.map(item => {
+    if (item.tier === props.tier) {
+      console.log(props.tier);
+      return (
+        <div className="product">
+          <img src={item.img_url} alt={item.name} />
+          <p>{item.name}</p>
+        </div>
+      );
+    }
+  });
+
+  return (
+    <section className="layout preview-layout">
+      <div className="disclaimer">
+        <h3>Preview</h3>
+        <div>You will receive a <strong><em>random assortment</em></strong> of snacks from the list below based on the <strong><em>selected tier</em></strong>.</div>
+      </div>
+      <div className="product-scroller">
+        {productList}
+      </div>
+    </section>
+  );
+}
--- client/src/components/Subscriptions.js
+++ client/src/components/Subscriptions.js
@@ -1,11 +1,63 @@
-import React from "react";
-import CheckoutForm from "./CheckoutForm";
+import React, { useState } from "react";
+import classNames from "classnames";
 
+import "./styles/Subscriptions.scss";
+// import CheckoutForm from "./CheckoutForm";
+// import Button from "./Button";
+import SnackPreview from "./SnackPreview";
+
+import tier1 from "../images/tier1_image.png";
+import tier2 from "../images/tier2_image.png";
+import tier3 from "../images/tier3_image.png";
 
 function Subscriptions() {
+  // const tierClass = classNames("button", "coiny", {
+  //   "button--orangy": orangy, //if props.orangy is true, append className
+  // });
+  const [tierInfo, setTierInfo] = useState(0);
+
+  const boxTiers = [
+    {
+      tier: 1,
+      price: 20,
+      quantity: 5,
+      img: tier1
+    },
+    {
+      tier: 2,
+      price: 40,
+      quantity: 8,
+      img: tier2
+    },
+    {
+      tier: 3,
+      price: 60,
+      quantity: 12,
+      img: tier3
+    }
+  ];
+
+  const tierCards = boxTiers.map(box => {
     return (
-        <CheckoutForm />
-    )
+      <div className="tier-card" onClick={() => setTierInfo(box.price)}>
+        <h2>Tier {box.tier}</h2>
+        <img src={box.img} alt="Snack" />
+        <p>{box.quantity} full-size snacks<br />from all over the world!</p>
+        <h3>{box.price}$/mo</h3>
+      </div>
+    );
+  });
+
+  return (
+    <section className="subs-layout layout">
+      <h1>Choose Your Tier!</h1>
+      <div className="tier-list">
+        {tierCards}
+      </div>
+      <SnackPreview tier={tierInfo} />
+    </section>
+    // <CheckoutForm />
+  )
 }
 
 export default Subscriptions;
