b5a13b471cab48f38578aa5653caa2af97067dba
Katie Liu <katieliu59@gmail.com>
2023-03-24 16:02:50 -0400

    Refactor code

--- client/src/components/OrderConfirmation.js
+++ client/src/components/OrderConfirmation.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Footer from "./Footer";
-import confirmation from './styles/confirmation.scss'
+import confirmation from "./styles/confirmation.scss";
 
 export default function OrderConfirmation(props) {
 	// console.log(props.cookieValue); // -> first_name
 	const [userData, setUserData] = useState(null);
 
-  useEffect(() => {
+	useEffect(() => {
 		async function fetchData() {
 			try {
 				const response = await axios.get("/order-summary");
@@ -18,72 +18,83 @@ export default function OrderConfirmation(props) {
 			}
 		}
 		fetchData();
-  }, []);
-  
-  //replace with pretty loader later
-  if (!userData) {
-    return <div>Loading...</div>;
-  }
+	}, []);
 
-  console.log(userData[0]); //logs the most recent order 
+	//replace with pretty loader later
+	if (!userData) {
+		return <div>Loading...</div>;
+	}
 
-   // handle button click which navigates to /account
-   const handleViewOrderHistory = () => {
-    window.location.href = "/account";
-   };
-  
+	console.log(userData[0]); //logs the most recent order
+
+	// handle button click which navigates to /account
+	const handleViewOrderHistory = () => {
+		window.location.href = "/account";
+	};
+
 	// if (userData[0].first_name === props.cookieValue) {
-		return (
-			<div className="c-order-confirmation">
-				<h1>Your order is complete!</h1>
-				<div>
-          <div className="c-order-confirmation__grid">
-            <span>
-            <h4>Order number: </h4>
-            <p>{userData[0].order_number}</p>
-            </span>
-					</div>
-          <div className="c-order-confirmation__grid">
-            <span>
+	return (
+		<div className="c-order-confirmation">
+			<h1>Your order is complete!</h1>
+			<div className="details-container">
+				<div className="c-order-confirmation__grid">
+					<span>
+						<h4>Order number: {userData[0].order_number}</h4>
+						{/* <p></p> */}
+					</span>
+				</div>
+				<div className="c-order-confirmation__grid">
+					<span>
 						<h4>Email address</h4>
 						<p>{userData[0].email}</p>
-            </span>
-					</div>
-					<div className="c-order-confirmation__grid">
+					</span>
+				</div>
+				<div className="c-order-confirmation__grid">
+					<div>
 						<h4>Shipping address</h4>
-						<p>{userData[0].street}</p>
-						<p>
-							{userData[0].city}, {userData[0].province}{" "}
-							{userData[0].postal_code}
-						</p>
-						<p>{userData[0].country}</p>
+						<div>
+							<p>{userData[0].street}</p>
+							<p>
+								{userData[0].city}, {userData[0].province}{" "}
+								{userData[0].postal_code}
+							</p>
+							<p>{userData[0].country}</p>
+						</div>
 					</div>
-					<div className="c-order-confirmation__grid">
-						<div className="c-order-confirmation__total">
-							<div className="c-order-confirmation__total-wrapper">
-								<div className="c-order-confirmation__label">
-									<p>{userData[0].subscription_tier} box</p>
-								</div>
+				</div>
+				<div className="c-order-confirmation__grid">
+					<div className="c-order-confirmation__total">
+						<div className="c-order-confirmation__total-wrapper">
+							<h4>Order summary</h4>
+							<div className="c-order-confirmation__label tier-wrapper">
+								<p>{userData[0].subscription_tier} box</p>
 								<div className="c-order-confirmation__value">
-									<p>{userData[0].price}</p>
-								</div>
+									<p>{userData[0].price}.00$</p>
+                </div>
+                <hr />
 							</div>
-							<div className="c-order-confirmation__total-wrapper">
-								<div className="c-order-confirmation__label">
-									<p>TOTAL</p>
-								</div>
+							<div className="c-order-confirmation__label total">
+								<p>TOTAL</p>
 								<div className="c-order-confirmation__value">
-									<p>{userData[0].price}</p>
+									<p>{userData[0].price}.00$</p>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-
-				<button className='button coiny button--orangy' onClick={handleViewOrderHistory}>View order history</button>
-				<Footer />
 			</div>
-		);
+			<div className="button-container">
+				<button
+					className="button coiny button--orangy"
+					onClick={handleViewOrderHistory}
+				>
+					View order history
+				</button>
+			</div>
+
+			<Footer />
+		</div>
+	);
 	// }
 	// else {
 	//     <h1>You do not have access to this page</h1>
