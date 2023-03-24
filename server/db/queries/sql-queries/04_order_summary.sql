6065c576e3575b5017357e309b4174acc49cdd75
Katie Liu <katieliu59@gmail.com>
2023-03-23 20:10:59 -0400

    Format query

--- server/db/queries/sql-queries/04_order_summary.sql
+++ server/db/queries/sql-queries/04_order_summary.sql
@@ -1,5 +1,20 @@
-SELECT users.id AS user_id, email, street, city, province, country, postal_code, subscription_tier, boxes.id AS order_number
-FROM users
-INNER JOIN boxes ON users.id = boxes.customer_id
--- WHERE users.id=1
-ORDER BY order_number DESC;
+SELECT
+    users.id AS user_id,
+    first_name,
+    last_name,
+    email,
+    street,
+    city,
+    province,
+    country,
+    postal_code,
+    subscription_tier,
+    boxes.price AS price,
+    boxes.id AS order_number,
+    boxes.order_date AS order_date
+FROM
+    users
+    INNER JOIN boxes ON users.id = boxes.customer_id
+ORDER BY
+    order_number DESC;
+
