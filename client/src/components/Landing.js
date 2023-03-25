5a85d2d37eeeec61d928a1c957121826305cb9ff
--replace-all <samma118@hotmail.com>
2023-03-25 00:35:46 -0400

    Modify position of landing page elements

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -11,27 +11,27 @@ import snack2 from "../images/snack2.png";
 export default function Landing() {
 
   return (
-    <section className="landing-background landing-layout layout">
-      <div className="coiny">
-        <h1>SNAZZY SNACKS</h1>
-      </div>
-      <div>
-        <h3>Upgrade your snacking game with pizazz-packed treats.<br />Every month at your doorstep!</h3>
-      </div>
-      <div>
-        <h5>*starting at 20$/mo</h5>
-      </div>
-      <div>
-        <Link to="/login">
-          <Button>GET SNACKING!</Button>
-        </Link>
-      </div>
-      <div className="landing-img">
-        <img src={candy} alt="Candy" />
+    <section className="landing-background layout">
+      <div className="landing-layout">
+        <div className="coiny">
+          <h1>SNAZZY SNACKS</h1>
+        </div>
+        <div>
+          <h3>Upgrade your snacking game with pizazz-packed treats.<br />Every month at your doorstep!</h3>
+        </div>
+        <div>
+          <h5>*starting at 20$/mo</h5>
+        </div>
+        <div>
+          <Link to="/login">
+            <Button>GET SNACKING!</Button>
+          </Link>
+        </div>
+        <img className="landing-candy" src={candy} alt="Candy" />
         <img className="landing-snack" src={snack} alt="Snack" />
+        <img className="landing-chip" src={chips} alt="Chips" />
+        <img className="landing-bar" src={snack2} alt="Snack" />
       </div>
-      <img className="landing-chip" src={chips} alt="Chips" />
-      <img className="landing-bar" src={snack2} alt="Snack" />
     </section>
   );
 }
