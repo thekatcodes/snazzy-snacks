ef40cc7a52ae9ef3233d943b02c0d072cf0855c5
Katie Liu <katieliu59@gmail.com>
2023-03-24 20:19:55 -0400

    Create logic to set price id accordingly

--- client/src/components/SnackPreview.js
+++ client/src/components/SnackPreview.js
@@ -1,46 +1,72 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
-import CheckoutForm from "./CheckoutForm";
+// import CheckoutForm from "./CheckoutForm";
+import Button from "./Button";
+
 
 import "./styles/SnackPreview.scss";
 // import classNames from "classnames";
 
-
 export default function SnackPreview(props) {
-  const [products, setProducts] = useState([]);
+	const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    axios.get("/products")
-      .then(response => {
-        setProducts(response.data)
-      })
-      .catch(error => console.log(error))
-  }, []);
+	useEffect(() => {
+		axios
+			.get("/products")
+			.then((response) => {
+				setProducts(response.data);
+			})
+			.catch((error) => console.log(error));
+	}, []);
 
-  // console.log(products);
+	console.log(products);
 
-  const productList = products.map(item => {
-    if (item.tier === props.tier) {
-      return (
-        <div className="product">
-          <img src={item.img_url} alt={item.name} />
-          <p>{item.name}</p>
-        </div>
-      );
-    }
-  });
+	let priceId;
 
-  return (
-    <section className="layout preview-layout">
-      <div className="disclaimer">
-        <h3>Preview</h3>
-        <div>You will receive a <strong><em>random assortment</em></strong> of snacks from the list below based on the <strong><em>selected tier</em></strong>.</div>
-      </div>
-      <div className="product-scroller">
-        {productList}
-      </div>
-      <CheckoutForm />
-    </section>
-  );
+	const productList = products.map((item) => {
+		if (item.tier === props.tier) {
+            return (
+                <div className="product">
+					<img
+						src={item.img_url}
+						alt={item.name}
+                        />
+					<p>{item.name}</p>
+				</div>
+			);
+		}
+        if (item.tier === 20) {
+            priceId = "price_1MncSlGdWagE6Ui8ya3zUfDR";
+        } else if (item.tier === 40) {
+            priceId = "price_1MncT2GdWagE6Ui89i2sgMdO";
+        } else if (item.tier === 60) {
+            priceId = 'price_1MncTLGdWagE6Ui8Q4pi6t4j';
+        }
+	});
+    
+    //console.log('PRICE ID PLS', priceId)
+    //tier 1: price_1MncSlGdWagE6Ui8ya3zUfDR
+    //tier 2: price_1MncT2GdWagE6Ui89i2sgMdO
+    //tier 3: price_1MncTLGdWagE6Ui8Q4pi6t4j
+
+	return (
+		<section className="layout preview-layout">
+			<div className="disclaimer">
+				<h3>Preview</h3>
+				<div>
+					You will receive a{" "}
+					<strong>
+						<em>random assortment</em>
+					</strong>{" "}
+					of snacks from the list below based on the{" "}
+					<strong>
+						<em>selected tier</em>
+					</strong>
+					.
+				</div>
+			</div>
+			<div className="product-scroller">{productList}</div>
+		</section>
+	);
 }
