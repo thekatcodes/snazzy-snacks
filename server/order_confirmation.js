c88f34d204f910d406599518ce6962979196e1a5
Katie Liu <katieliu59@gmail.com>
2023-03-26 13:35:22 -0400

    Update code, move subscription tier from updateAddress to createOrderNumbmer

--- server/order_confirmation.js
+++ server/order_confirmation.js
@@ -16,12 +16,12 @@ async function findUserId(email) {
   }
 
 // Updates users table with address
-async function updateAddress(street, city, province, countryName, postalCode, subscriptionTier, subscriptionId, email) {
+async function updateAddress(street, city, province, countryName, postalCode, subscriptionId, email) {
     try {
       await pool.query(`
-        UPDATE users SET street=$1, city=$2, province=$3, country=$4, postal_code=$5, subscription_tier=$6, stripe_sub_id=$7
-        WHERE email LIKE $8
-      `, [street, city, province, countryName, postalCode, subscriptionId, subscriptionTier, email]);
+        UPDATE users SET street=$1, city=$2, province=$3, country=$4, postal_code=$5, stripe_sub_id=$6
+        WHERE email LIKE $7
+      `, [street, city, province, countryName, postalCode, subscriptionId, email]);
       return "Address updated successfully";
     } catch (err) {
       console.log(err);
@@ -29,13 +29,13 @@ async function updateAddress(street, city, province, countryName, postalCode, su
 }
   
 // Updates boxes table with new row (order number)
-async function createOrderNumber(userId, price, formattedDate) {
+async function createOrderNumber(userId, subscriptionTier, price, formattedDate) {
     try {
       const id = await userId;
       await pool.query(`
-        INSERT INTO boxes (customer_id, price, order_date)
-        VALUES ($1, $2, $3);
-      `, [id, price, formattedDate]);
+        INSERT INTO boxes (customer_id, subscription_tier, price, order_date)
+        VALUES ($1, $2, $3, $4);
+      `, [id, subscriptionTier, price, formattedDate]);
       console.log("Order number created successfully");
     } catch (err) {
       console.log("Error creating boxes table row", err);
@@ -44,9 +44,9 @@ async function createOrderNumber(userId, price, formattedDate) {
   
 // Grab order summary for GET request on the front-end
 async function orderSummary() {
-          try {
-            const order = await pool.query(`
-            SELECT users.id AS user_id, first_name, last_name, email, street, city, province, country, postal_code, subscription_tier, boxes.price AS price, boxes.id AS order_number, boxes.order_date AS order_date
+        try {
+          const order = await pool.query(`
+            SELECT users.id AS user_id, first_name, last_name, email, street, city, province, country, postal_code, boxes.subscription_tier AS subscription_tier, boxes.price AS price, boxes.id AS order_number, boxes.order_date AS order_date
             FROM users
             INNER JOIN boxes ON users.id = boxes.customer_id
             ORDER BY order_number DESC;
@@ -54,10 +54,9 @@ async function orderSummary() {
     
             console.log('Order summary ready');
             return order.rows;
-           } catch (error) {
-              console.log(error);
-              throw new Error('Could not retrieve order summary');
-
+        } catch (error) {
+            console.log(error);
+            throw new Error('Could not retrieve order summary');
         }
     }
 
