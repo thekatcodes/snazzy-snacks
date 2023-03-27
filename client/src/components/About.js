f2751d1e0f1c19f70bef0ff7ae5463adcfddb356
Katie Liu <katieliu59@gmail.com>
2023-03-26 21:52:45 -0400

    Add about text

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,16 +1,29 @@
 import React from "react";
 
-
 export default function About() {
-
-    return (
-
-        <div>
-            <h1>About us</h1>
+	return (
+		<div>
+			<h1>About us</h1>
+			<p>
+				Welcome to Snazzy Snacks, the snack subscription service that's here to
+				satisfy your cravings and put a smile on your face! We know that
+				snacking is serious business, and we take it just as seriously as you
+				do. At Snazzy Snacks, we believe that snacking should be an adventure.
+				That's why we've scoured the world for the most delicious, unusual, and
+				downright quirky snacks we could find. From spicy chips to sweet treats,
+				we've got it all, and we're constantly on the hunt for the next big
+				thing in snacking. So if you're ready to join the Snazzy Snacks party,
+				sign up today and let us take care of your snacking needs. Who knows,
+				you might even discover your new favorite snack!
+			</p>
             <h2>Meet the team</h2>
-            <h2>How it works</h2>
-        </div>
-    )
-    
+            <div>
+                <div>
 
+                </div>
+            </div>
+            
+			<h2>How it works</h2>
+		</div>
+	);
 }
