ab0f1dab61d36fcb6614d3b6a52caadb3c94a420
Katie Liu <katieliu59@gmail.com>
2023-03-26 22:17:41 -0400

    Update code

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Button from "./Button";
 
-
 import katie from "../images/Katie.png";
 import lyon from "../images/Lyon.png";
 import samma from "../images/Samma.png";
 import account from "../images/account.png";
 import packaging from "../images/packaging.png";
 import surprise_box from "../images/surprise_box.png";
+
+
+
 
 export default function About(props) {
 	return (
@@ -18,13 +20,19 @@ export default function About(props) {
 				Welcome to Snazzy Snacks, the snack subscription service that's here to
 				satisfy your cravings and put a smile on your face! We know that
 				snacking is serious business, and we take it just as seriously as you
-				do. At Snazzy Snacks, we believe that snacking should be an adventure.
+				do.
+			</p>
+			<p>
+				At Snazzy Snacks, we believe that snacking should be an adventure.
 				That's why we've scoured the world for the most delicious, unusual, and
 				downright quirky snacks we could find. From spicy chips to sweet treats,
 				we've got it all, and we're constantly on the hunt for the next big
-				thing in snacking. So if you're ready to join the Snazzy Snacks party,
-				sign up today and let us take care of your snacking needs. Who knows,
-				you might even discover your new favorite snack!
+				thing in snacking.
+			</p>
+			<p>
+				So if you're ready to join the Snazzy Snacks party, sign up today and
+				let us take care of your snacking needs. Who knows, you might even
+				discover your new favorite snack!
 			</p>
 			<h2>Meet the team</h2>
 			<div>
@@ -65,38 +73,43 @@ export default function About(props) {
 					/>
 					<h3>Create account</h3>
 					<p>Create a customer account to keep track of your subscription</p>
-                </div>
-                <div>
+				</div>
+				<div>
 					<img
 						className="packaging-how"
 						src={packaging}
 						alt="Three packages"
 					/>
 					<h3>Select a size</h3>
-					<p>Choose between three sizes of monthly boxes: Tier 1, Tier 2, or Tier 3</p>
-                </div>
-                <div>
+					<p>
+						Choose between three sizes of monthly boxes: Tier 1, Tier 2, or Tier
+						3
+					</p>
+				</div>
+				<div>
 					<img
 						className="surprise-how"
 						src={surprise_box}
 						alt="Surprise box"
 					/>
 					<h3>Select a size</h3>
-					<p>Choose between three sizes of monthly boxes: Tier 1, Tier 2, or Tier 3</p>
-                </div>
-            </div>
-            <div>
-        {props.cookieValue ? 
-          <Link to="/account">
-            <Button>GET SNACKING!</Button>
-          </Link>
-        :
-          <Link to="/login">
-            <Button>GET SNACKING!</Button>
-          </Link>
-        }
-        
-      </div>
+					<p>
+						Choose between three sizes of monthly boxes: Tier 1, Tier 2, or Tier
+						3
+					</p>
+				</div>
+			</div>
+			<div>
+				{props.cookieValue ? (
+					<Link to="/account">
+						<Button>GET SNACKING!</Button>
+					</Link>
+				) : (
+					<Link to="/login">
+						<Button>GET SNACKING!</Button>
+					</Link>
+				)}
+			</div>
 		</div>
 	);
 }
