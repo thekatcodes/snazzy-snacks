8ecf926020ee1cb847dceaa42d9de4b094251cb3
Katie Liu <katieliu59@gmail.com>
2023-03-26 21:02:40 -0400

    Create about component

---
+++ client/src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export default function About() {
+
+    return (
+
+        <div>
+            <h1>About us</h1>
+            <h2>Meet the team</h2>
+            <h2>How it works</h2>
+        </div>
+    )
+    
+
+}
