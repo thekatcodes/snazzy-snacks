81ef0e3ddeb2b57a22594e1f43563ac1c1c3054d
Katie Liu <katieliu59@gmail.com>
2023-03-27 20:17:35 -0400

    Update current address style and refactor code

--- client/src/components/Address.js
+++ client/src/components/Address.js
@@ -91,9 +91,11 @@ const Address = (props) => {
               <div className="address-title">Update Address</div>
               <br />
               <div>Current Address</div>
-              <div>{userData[index-1].street}</div>
-              <div>{userData[index-1].city}, {userData[index-1].province}</div>
-              <div>{userData[index-1].postal_code}</div>
+              <div className="current-address-container">  
+                <div>{userData[index-1].street}</div>
+                <div>{userData[index-1].city}, {userData[index-1].province}</div>
+                <div>{userData[index-1].postal_code}</div>
+              </div>
               <form className="form-layout" onSubmit={updateAddress}>
                 <label htmlFor="street"/>
                 <input 
